1cee2dcb12a6a4732b8dac0dbf9e8760773b1be5
Сергей Назаров <nazaroff.serezha2014@gmail.com>
2023-05-04 18:18:51 +0300

    Добавил ph-pages

--- reactpapptypescript/src/dataGenerator/companies.ts
+++ reactpapptypescript/src/dataGenerator/companies.ts
@@ -1,4 +1,4 @@
-import { company, internet, address, phone, random } from 'faker/locale/en_US';
+import { company, internet, address, phone, random } from 'faker/locale/ru_RU';
 import { randomDate } from './utils';
 
 import { Db } from './types';
